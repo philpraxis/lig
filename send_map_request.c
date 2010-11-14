2180dfec80f659a60d1fe44f1d0b063a65a971d9
David Meyer <dmm@wayback.(none)>
2010-11-14 12:51:48 -0800

    Change licensing terms.

--- send_map_request.c
+++ send_map_request.c
@@ -13,25 +13,31 @@
  *	IPv6 support added by Lorand Jakab <lj@icanhas.net>
  *	Mon Aug 23 15:26:51 2010 +0200
  *
- *	This program is free software; you can redistribute it
- *	and/or modify it under the terms of the GNU General
- *	Public License as published by the Free Software
- *	Foundation; either version 2 of the License, or (at your
- *	option) any later version. 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     o Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     o Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     o Neither the name of the University nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
  *
- *	This program is distributed in the hope that it will be
- *	useful,  but WITHOUT ANY WARRANTY; without even the
- *	implied warranty of MERCHANTABILITY or FITNESS FOR A
- *	PARTICULAR PURPOSE.  See the GNU General Public License
- *	for more details. 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	You should have received a copy of the GNU General Public
- *	License along with this program; if not, write to the
- *	Free Software Foundation, Inc., 59 Temple Place - Suite
- *	330, Boston, MA  02111-1307, USA. 
  *
  *
- *	 $Header: /home/dmm/lisp/lig/RCS/send_map_request.c,v 1.61 2009/10/13 23:35:40 dmm Exp $ 
+ *	 $Header: /mnt/disk1/dmm/src/lig/RCS/send_map_request.c,v 1.1 2010/11/14 20:48:35 dmm Exp $ 
  *
  */
 
@@ -66,7 +72,7 @@
  *	dmm@1-4-5.net
  *	Thu Apr 16 14:46:51 2009
  *
- *	$Header: /home/dmm/lisp/lig/RCS/send_map_request.c,v 1.61 2009/10/13 23:35:40 dmm Exp $
+ *	$Header: /mnt/disk1/dmm/src/lig/RCS/send_map_request.c,v 1.1 2010/11/14 20:48:35 dmm Exp $
  *
  */
 
